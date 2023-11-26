4196affa0d24351ca2a2d1e77c2b4f42a405b6a7
12andrian85 <118369529+12andrian85@users.noreply.github.com>
2023-11-26 02:49:32 +0200

    git init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Import the Bootstrap bundle
+//
+// Includes Popper and all of Bootstrap's JS plugins.
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Custom JS here
+console.log('hello')
